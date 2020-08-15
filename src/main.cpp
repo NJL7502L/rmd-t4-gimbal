f40f767fbba91fc8c17bd0e937b60b44cd432d1d
NJL7502L <g.hana1211.tako@gmail.com>
2020-08-15 13:36:22 +0900

    コンパイル通った。

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
+#include "../lib/FlexCAN_T4_manager/FlexCAN_T4_manager.h"
+
+FlexCAN_T4_manager* canmanager = FlexCAN_T4_manager::getInstance();
 
 void setup(){
+  canmanager->init(false,false,true);
   pinMode(LED_BUILTIN,OUTPUT);
 }
 
@@ -9,4 +13,8 @@ void loop(){
   digitalWrite(LED_BUILTIN,HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN,LOW);
+  uint32_t canid = 0;
+  uint8_t data[8] = {};
+  canmanager->readAll();
+  canmanager->getBus1(canid,data);
 }
