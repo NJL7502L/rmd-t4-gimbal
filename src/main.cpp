e36e0cb284525f97d91bec4e6e5a528f5532a828
NJL7502L <g.hana1211.tako@gmail.com>
2020-08-15 15:57:38 +0900

    AliExpressのIMUをライブラリ化することができた。

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,14 @@
 #include <Arduino.h>
 #include <MsTimer2.h>
 #include "../lib/FlexCAN_T4_manager/FlexCAN_T4_manager.h"
+#include "../lib/AliExpressIMU_manager/AliExpressIMU_manager.h"
 
 FlexCAN_T4_manager* canmanager = FlexCAN_T4_manager::getInstance();
-
-union FtoC{
-  float f;
-  uint8_t c[4];
-};
-FtoC pitch, roll, yaw;
+AliExpressIMU_manager IMU;
 
 void timerInt(){
-  uint8_t data[8] = {};
   canmanager->readBus3();
-
-  canmanager->getBus3(0x0A,data);
-  pitch.c[0] = data[0];
-  pitch.c[1] = data[1];
-  pitch.c[2] = data[2];
-  pitch.c[3] = data[3];
-  roll.c[0] = data[4];
-  roll.c[1] = data[5];
-  roll.c[2] = data[6];
-  roll.c[3] = data[7];
-  canmanager->getBus3(0x0B,data);
-  yaw.c[0] = data[0];
-  yaw.c[1] = data[1];
-  yaw.c[2] = data[2];
-  yaw.c[3] = data[3];
+  IMU.read();
 }
 
 void setup(){
@@ -48,9 +29,9 @@ void printTitleValue(const char* str,float val){
 }
 
 void debugPrint(){
-  printTitleValue("yaw",yaw.f);
-  printTitleValue("pitch",pitch.f);
-  printTitleValue("roll",roll.f);
+  printTitleValue("yaw",IMU.yaw);
+  printTitleValue("pitch",IMU.pitch);
+  printTitleValue("roll",IMU.roll);
   Serial.println();
 }
 
