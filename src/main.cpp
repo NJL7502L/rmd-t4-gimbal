2165902476e3546a3cf205320e07de4e3d1cdfef
NJL7502L <g.hana1211.tako@gmail.com>
2020-08-17 18:59:59 +0900

    auto format

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,43 @@
+#include "../lib/AliExpressIMU_manager/AliExpressIMU_manager.h"
+#include "../lib/FlexCAN_T4_manager/FlexCAN_T4_manager.h"
+#include "../lib/GyemsRMD_manager/GyemsRMD_manager.h"
 #include <Arduino.h>
 #include <MsTimer2.h>
-#include "../lib/FlexCAN_T4_manager/FlexCAN_T4_manager.h"
-#include "../lib/AliExpressIMU_manager/AliExpressIMU_manager.h"
-#include "../lib/GyemsRMD_manager/GyemsRMD_manager.h"
 
-FlexCAN_T4_manager* canmanager = FlexCAN_T4_manager::getInstance();
+FlexCAN_T4_manager *canmanager = FlexCAN_T4_manager::getInstance();
 // AliExpressIMU_manager imu;
 GyemsRMD_manager motor;
 
-void timerInt(){
+void timerInt() {
   canmanager->readBus3();
   // imu.read(canmanager);
   motor.read(canmanager);
 
-
   uint8_t buf[8] = {0};
   buf[0] = 0x9C;
-  canmanager->pushBus3(0x141,buf);
+  canmanager->pushBus3(0x141, buf);
   canmanager->writeAll();
 }
 
-void setup(){
-  canmanager->init(false,false,true);
+void setup() {
+  canmanager->init(false, false, true);
   Serial.begin(115200);
 
-  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   motor.setId(1);
 
   MsTimer2::set(1, timerInt);
   MsTimer2::start();
 }
 
-template <typename T>
-void printTitleValue(const char* str,T val){
+template <typename T> void printTitleValue(const char *str, T val) {
   Serial.print(str);
   Serial.print(" :\t");
   Serial.print(val);
   Serial.print("\t");
 }
 
-void debugPrint(){
+void debugPrint() {
   // printTitleValue("yaw",imu.yaw);
   // printTitleValue("pitch",imu.pitch);
   // printTitleValue("roll",imu.roll);
@@ -62,17 +60,17 @@ void debugPrint(){
   // printTitleValue("LVProtection",motor.gotData.information.error.lowVoltageProtection);
   // printTitleValue("OTProtection",motor.gotData.information.error.overTemperatureProtection);
 
-  printTitleValue("temperature",motor.gotData.information.temperature);
-  printTitleValue("speed",motor.gotData.speed.current);
-  printTitleValue("torque iq",motor.gotData.torque.iq);
-  printTitleValue("encoader",motor.gotData.encoder.current);
+  printTitleValue("temperature", motor.gotData.information.temperature);
+  printTitleValue("speed", motor.gotData.speed.current);
+  printTitleValue("torque iq", motor.gotData.torque.iq);
+  printTitleValue("encoader", motor.gotData.encoder.current);
   Serial.println();
 }
 
-void loop(){
+void loop() {
   delay(100);
-  digitalWrite(LED_BUILTIN,HIGH);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(100);
-  digitalWrite(LED_BUILTIN,LOW);
+  digitalWrite(LED_BUILTIN, LOW);
   debugPrint();
 }
